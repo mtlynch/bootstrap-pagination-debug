6aed349378f3042b78c43e2203901d46489571a6
Michael Lynch <git@mtlynch.io>
2019-06-04 10:50:39 -0400

    Updating with proof of concept of bug

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,69 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <b-pagination-nav
+      :pages="pages"
+      :number-of-pages="numberOfPages"
+      v-model="currentPage"
+      no-page-detect
+      use-router
+    ></b-pagination-nav>
+    <b-form-checkbox v-model="showOddNumbers">Show odd numbers</b-form-checkbox>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  name: "home",
+  data() {
+    return {
+      showOddNumbers: false
+    };
+  },
+  computed: {
+    pages: function() {
+      const pages = [];
+      for (let i = 10; i <= 15; i++) {
+        if (!this.showOddNumbers && i % 2 == 1) {
+          continue;
+        }
+        pages.push({
+          link: "" + i,
+          text: "" + i
+        });
+      }
+      return pages;
+    },
+    numberOfPages: function() {
+      const len = this.pages.length;
+      console.log(`numberOfPages is returning ${len}`);
+      return len;
+    },
+    currentPage: {
+      get: function() {
+        const pages = this.pages;
+        for (let i = 0; i < pages.length; i++) {
+          const page = this.pages[i];
+          if (this.$route.params.pageNumber == +page.text) {
+            const ans = i + 1;
+            console.log(`returning ${ans} from currentPage`);
+            return ans;
+          }
+        }
+        const index = 1;
+        console.log(
+          `couldn't find matching page: returning ${index} from currentPage`
+        );
+        return index;
+      },
+      set: function(newValue) {
+        console.log(`Setting currentPage to ${newValue}`);
+      }
+    }
+  },
+  watch: {
+    showOddNumbers: function(val) {
+      console.log(`setting showOddNumbers to ${val}`);
+    }
   }
-}
+};
 </script>
